9ec7ba67b56e9e9887a1bc662f0e32ec85b22f84
Rakshith <rakkshith2001@gmail.com>
2023-07-03 22:40:42 +0530

    added successfully

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const connectDB = require('./config/dbConn')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
 
-
 console.log(process.env.NODE_ENV)
 
 connectDB()
@@ -32,7 +31,19 @@ app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/notes', require('./routes/noteRoutes'))
 
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
 app.all('*', require('./WrongURL/errorworngURL'))
+
 
 app.use(errorHandler)
 
@@ -45,3 +56,7 @@ mongoose.connection.on('error', err => {
     console.log(err)
     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
+
+
+
+
