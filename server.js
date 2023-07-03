0f5773740ab21881394b01da762d7da5001c233e
Rakshith <rakkshith2001@gmail.com>
2023-07-03 22:42:08 +0530

    added successfully

--- server.js
+++ server.js
@@ -31,16 +31,7 @@ app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/notes', require('./routes/noteRoutes'))
 
-app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
+
 
 app.all('*', require('./WrongURL/errorworngURL'))
 
